8e0e2027521d54b04d77bed8200124809f7bfb0d
biel-git <gabrielsljobs@hotmail.com>
2024-09-12 21:11:29 +0100

    Basic JavaScript Projects Assignment

---
+++ BasicJSProjects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+//main.js
+
+window.alert("Hello, this is part of my assignment");  //This will display a window alert on the web page
+
+document.write("For the last time, I'm doing this for my assignment!"); //This will display text on the web page 
+
+// Two variables requirement
+var Sent1 = "This is the begining of the end";
+var Sent2 = " and this is the end of the beginning";
+
+// both variables together
+document.write(Sent1+Sent2);
+
+//Writing an expression
+document.write(100*5)
+document.write("Yes, I have 500 cats"); //This was not required :D
