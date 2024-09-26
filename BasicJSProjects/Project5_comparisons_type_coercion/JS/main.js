5af1f6e71376bab76c8cc27694205f772bf6b181
biel-git <gabrielsljobs@hotmail.com>
2024-09-26 18:47:52 +0100

    Project 5

---
+++ BasicJSProjects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,33 @@
+//Boolean Assignment
+var x = true;
+
+//Type Of Operator Assignment
+document.write(typeof x);
+document.write("<br>");
+
+document.write("10" + 10);
+document.write("<br>");
+
+
+//Double Equal Signs Assignment
+document.write(5 == 5);
+document.write("<br>");
+
+//Triple Equal Signs Assignment
+x = 1;
+y = 1;
+document.write(x === y);
+document.write("<br>");
+
+//And Operator Assignment
+document.write(10 > 5 && 11 > 6);
+document.write("<br>");
+
+document.write(10 < 5 || 11 < 6);
+document.write("<br>");
+
+
+//Not Operator Assignment
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(1>2);
+}
